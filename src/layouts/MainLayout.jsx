2cd17e7ab25434aff01bba0eb7c358d907b847a5
hiram.rubio <floreshiram03@gmail.com>
2022-08-11 20:08:27 -0500

    feature: udpated the app navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -17,40 +17,18 @@ function MainLayout() {
     }
   }, [cookies]);
   return (
-    <div className="flex flex-row flex-wrap md:h-screen">
-      <nav className="w-full md:w-64 flex flex-row md:flex-col items-center justify-between p-5 md:p-0 md:bg-[#5C509B] ">
-        <img
-          className="w-7 md:hidden cursor-pointer"
-          src={HamburgerMenu}
-          alt=""
-        />
-        <div className="hidden md:flex flex-col py-10 text-white text-center w-full">
-          <img src={Alien} alt="" className="m-auto w-36 mb-5" />
-          <p className="text-2xl font-medium mb-1.5">Alien Alieno</p>
-          <p className="mb-14">ID75315926</p>
-          <div className="menu-item flex flex-row items-center justify-center p-3 hover:bg-[#7D73B2] cursor-pointer">
-            <Piggy fill="white" alt="" className="w-7 mr-5 item-image-hover" />
-            <p className="w-24 text-left"><Link to="/incomes">Mis ingresos</Link></p>
-          </div>
-          <div className="menu-item flex flex-row items-center justify-center p-3 hover:bg-[#7D73B2] cursor-pointer">
-            <MoneyCheck
-              fill="white"
-              alt=""
-              className="w-7 mr-5 item-image-hover"
-            />
-            <p className="w-24 text-left"><Link to="/expenses">Mis gastos</Link></p>
-          </div>
-          <div
-            className="menu-item flex flex-row items-center justify-center p-3 hover:bg-[#7D73B2] cursor-pointer mt-36"
-            onClick={() => {
-              cookies.remove("TOKEN", { path: "/" });
-              window.location.href = "/";
-            }}
-          >
-            <Logout fill="white" alt="" className="w-7 mr-5 item-image-hover" />
-            <p className="w-24 text-left">Salir</p>
-          </div>
-        </div>
+    <div className="">
+      <nav className="flex flex-row flex-wrap items-center justify-evenly bg-[#5C509B] text-white p-5">
+        <p className="w-24 text-left hover:underline cursor-pointer">
+          <Link to="/incomes">Inicio</Link>
+        </p>
+        <p className="w-24 text-left hover:underline cursor-pointer">
+          <Link to="/incomes">Mis ingresos</Link>
+        </p>
+        <p className="w-24 text-left hover:underline cursor-pointer">
+          <Link to="/expenses">Mis gastos</Link>
+        </p>
+        <p className="w-24 text-left hover:underline cursor-pointer">Salir</p>
       </nav>
     </div>
   );
