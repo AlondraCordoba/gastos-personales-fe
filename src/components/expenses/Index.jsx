ab5f654478c42d75325658701d6ab9ea4325d725
hiram.rubio <floreshiram03@gmail.com>
2022-08-12 03:29:20 -0500

    feat: addde the expenses  modules complete functionality

--- src/components/expenses/Index.jsx
+++ src/components/expenses/Index.jsx
@@ -3,6 +3,9 @@ import { Data } from "../../context/data-context";
 import axios from 'axios';
 
 function Index() {
+  const [showCreateModal, setCreateModal] = useState(false);
+  const [showEditModal, setEditModal] = useState(false);
+  const [showEditId, setEditId] = useState("");
   const [info, setInfo] = useState([]);
   const id = localStorage.getItem("ID");
   const data = Data();
@@ -37,12 +40,14 @@ function Index() {
   const postCategory = () => {
     axios.post(process.env.REACT_APP_MONGO + '/post-category', model).then((result) => {
       getCategory();
+      setCreateModal(false);
     });
   }
 
   const putCategory = () => {
     axios.put(process.env.REACT_APP_MONGO + '/put-category', model).then((result) => {
       getCategory();
+      setEditModal(false);
     });
   }
 
@@ -54,30 +59,226 @@ function Index() {
   }
 
   return(
-    <div>
-      <div style={{display: 'flex'}}>
-        {
-          info.length > 0 && info.map((result, index) => (
-            result && result.type == "expense" ? (
-              <div key={index} style={{height: '220px', background: 'gray', width: '200px', color: 'white'}}>
-                {result.name}<br/>
-                {result.monto}<br/>
-                <input style={{color: 'black'}} onChange={(e) => {model.name = e.target.value}} placeholder="Descripcion"/><br/>
-                <input style={{color: 'black'}} onChange={(e) => {model.monto = e.target.value}} placeholder="Monto"/><br/>
-                <button onClick={() => {model.id = result._id; putCategory()}}>Editar</button>
-                <button onClick={() => {deleteCategory(result._id)}}>Eliminar</button>
+    <>
+      <div className="p-5 md:p-10">
+        <div className="flex flex-row justify-center">
+          <div className="w-full md:w-1/2 flex flex-row flex-wrap items-center">
+            <p className="w-full md:w-1/2 text-center text-4xl md:mb-0 mb-5">
+              Mis gastos
+            </p>
+            <button
+              className="w-full md:w-1/2 flex justify-center bg-[#DCDAE4] hover:bg-[#5C509B] hover:text-white rounded"
+              onClick={() => setCreateModal(true)}
+            >
+              Nuevo gasto
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {showCreateModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-6 max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-xl md:text-3xl">Agregar nuevo gasto</h3>
+                </div>
+                {/*body*/}
+                <div className="p-5">
+                  <div className="flex flex-col mb-5">
+                    <label className="mb-3 font-semibold">Descripción:</label>
+                    <input
+                      onChange={(e) => {
+                        model.name = e.target.value;
+                      }}
+                      className="form-control
+                      block
+                      w-full
+                      px-3
+                      py-1.5
+                      text-base
+                      font-normal
+                      text-gray-700
+                      bg-white bg-clip-padding
+                      border-2 border-gray-300
+                      rounded
+                      transition
+                      ease-in-out
+                      m-0
+                      focus:text-gray-700 focus:bg-white focus:border-[#5C509B] focus:outline-none"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="mb-3 font-semibold">Monto:</label>
+                    <input
+                      onChange={(e) => {
+                        model.monto = e.target.value;
+                      }}
+                      className="form-control
+                      block
+                      w-full
+                      px-3
+                      py-1.5
+                      text-base
+                      font-normal
+                      text-gray-700
+                      bg-white bg-clip-padding
+                      border-2 border-gray-300
+                      rounded
+                      transition
+                      ease-in-out
+                      m-0
+                      focus:text-gray-700 focus:bg-white focus:border-[#5C509B] focus:outline-none"
+                    />
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 bg-transparent uppercase px-6 py-3 text-sm outline-none rounded hover:bg-red-500 hover:text-white m-1"
+                    type="button"
+                    onClick={() => setCreateModal(false)}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    className="text-emerald-500 bg-transparent uppercase px-6 py-3 text-sm outline-none rounded hover:bg-emerald-500 hover:text-white m-1"
+                    type="button"
+                    onClick={() => postCategory()}
+                  >
+                    Agregar
+                  </button>
+                </div>
               </div>
-            ): null
-          ))
-        }
+            </div>
+          </div>
+        </>
+      ) : null}
+
+      {showEditModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-6 max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-xl md:text-3xl">Editar gasto</h3>
+                </div>
+                {/*body*/}
+                <div className="p-5">
+                  <div className="flex flex-col mb-5">
+                    <label className="mb-3 font-semibold">Descripción:</label>
+                    <input
+                      onChange={(e) => {
+                        model.name = e.target.value;
+                      }}
+                      className="form-control
+                      block
+                      w-full
+                      px-3
+                      py-1.5
+                      text-base
+                      font-normal
+                      text-gray-700
+                      bg-white bg-clip-padding
+                      border-2 border-gray-300
+                      rounded
+                      transition
+                      ease-in-out
+                      m-0
+                      focus:text-gray-700 focus:bg-white focus:border-[#5C509B] focus:outline-none"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="mb-3 font-semibold">Monto:</label>
+                    <input
+                      onChange={(e) => {
+                        model.monto = e.target.value;
+                      }}
+                      className="form-control
+                      block
+                      w-full
+                      px-3
+                      py-1.5
+                      text-base
+                      font-normal
+                      text-gray-700
+                      bg-white bg-clip-padding
+                      border-2 border-gray-300
+                      rounded
+                      transition
+                      ease-in-out
+                      m-0
+                      focus:text-gray-700 focus:bg-white focus:border-[#5C509B] focus:outline-none"
+                    />
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 bg-transparent uppercase px-6 py-3 text-sm outline-none rounded hover:bg-red-500 hover:text-white m-1"
+                    type="button"
+                    onClick={() => setEditModal(false)}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    className="text-emerald-500 bg-transparent uppercase px-6 py-3 text-sm outline-none rounded hover:bg-emerald-500 hover:text-white m-1"
+                    type="button"
+                    onClick={() => {
+                      model.id = showEditId;
+                      putCategory();
+                    }}
+                  >
+                    Editar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+      <div className="inline-flex flex-wrap p-5 md:p-10 justify-center">
+        {info.length > 0 &&
+          info.map((result, index) =>
+            result && result.type === "expense" ? (
+              <div
+                key={index}
+                className="bg-white shadow-lg w-full md:w-1/5 rounded-lg p-5 mb-5 md:m-5"
+              >
+                <div className="bg-white rounded mb-5">
+                  <p className="text-2xl font-semi-bold">{result.name}</p>
+                  <p className="font-light"><span>$</span>{result.monto}</p>
+                </div>
+                <div className="flex flex-row flex-wrap justify-end">
+                  <button
+                    className="text-red-500 bg-transparent uppercase px-6 py-3 text-sm outline-none rounded hover:bg-red-500 hover:text-white m-1"
+                    onClick={() => {
+                      deleteCategory(result._id);
+                    }}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="text-[#5C509B] bg-transparent uppercase px-6 py-3 text-sm outline-none rounded hover:bg-[#5C509B] hover:text-white m-1"
+                    type="button"
+                    onClick={() => {
+                      setEditModal(true);
+                      setEditId(result._id);
+                    }}
+                  >
+                    Editar
+                  </button>
+                </div>
+              </div>
+            ) : null
+          )}
       </div>
-      <hr style={{margin: '5% 1% 5% 1%'}}/>
-      <div className="agregar">
-          <input onChange={(e) => {model.name = e.target.value}} placeholder="Descripcion"/>
-          <input onChange={(e) => {model.monto = e.target.value}} placeholder="Monto"/>
-          <button onClick={() => postCategory()}>Agregar</button>
-      </div>
-    </div>
+    </>
   )
 }
   
