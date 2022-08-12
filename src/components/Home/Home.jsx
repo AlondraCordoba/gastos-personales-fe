4d3b39ce83b27cb86d050702086ed8c07a39d49e
ErikUTA <erik4tohj@gmail.com>
2022-08-11 23:50:07 -0500

    feat: aggregated connection to control category data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Data } from "../../context/data-context";
+// import { Data } from "../../context/data-context";
 
 
 function Home(props) {
-  const [info, setInfo] = useState([]);
-  const data = Data();
 
 
   return(
     <div className="homePage">
-      {
+      {/* {
         data && data.data.map((c, i) => (
-          <h1 key={i}>{c.name}</h1>
+          <button key={i}>{c.name}</button>
         ))
-      }
+      } */}
     </div>
   ) 
 }
